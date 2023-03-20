413a627ec1493f31d61d977b8bb2302b8f84e65a
OmairaBarrera <lomaira.barrera@gmail.com>
2023-03-20 18:36:31 -0500

    feat: :tada: Config Workspace
    Start menu of options

---
+++ js/main.js
@@ -0,0 +1,4 @@
+/*construir un menú en una función
+alert que gestione las siguientes opciones de menú referentes al
+manejo de los arrays, objetos, funciones y otras operaciones en
+JavaScript entre dichos temas. */
